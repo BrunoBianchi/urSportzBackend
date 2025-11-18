b9e288ab399b29a11551f5a8ec918ed94d21b135
BrunoBianchi <bruno.raiadobian@gmail.com>
2025-11-18 10:41:40 -0300

    backend

---
+++ dist/src/services/get-post.js
@@ -0,0 +1,20 @@
+import { AppDataSource } from "../database/postgres/data-source.js";
+import { Post } from "../database/postgres/entities/post-entity.js";
+const postRepository = AppDataSource.getRepository(Post);
+const defaultPostRelations = [
+    "author",
+    "parent",
+    "comments",
+    "comments.author",
+    "likes",
+    "likes.user",
+    "repostedBy"
+];
+export const getPost = async ({ id, relations }) => {
+    const post = await postRepository.findOne({
+        where: { id },
+        relations: relations ?? defaultPostRelations
+    });
+    return post ?? undefined;
+};
+//# sourceMappingURL=get-post.js.map
